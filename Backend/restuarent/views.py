32e1ba48cf48ddc288c01aa82c291de4164fa415
ismail <dhanto05@gmail.com>
2023-01-01 16:22:46 +0300

    commited

--- Backend/restuarent/views.py
+++ Backend/restuarent/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from .models import *
+from .serializers import *
+
+
+def index(request):
+    context={}
+    return render(request,"index.html",context)
+def home(request):
+    context={}
+    return render(request,"indexq.html",context)
+
+@api_view(['GET'])
+def get_products(request):
+
+    get_product = Product.objects.all()
+    get_serializer = Product_Serializer(get_product, many = True)
+
+    return JsonResponse({
+        "succsess": True,
+        "products":get_serializer.data
+
+    })
+
 
 # Create your views here.
