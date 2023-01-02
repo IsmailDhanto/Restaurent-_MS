85bc28654fd1199e88dc48630e2a02622edb1c4b
ismail <dhanto05@gmail.com>
2023-01-02 21:13:04 +0300

    commited

--- Backend/restuarent/views.py
+++ Backend/restuarent/views.py
@@ -5,12 +5,24 @@ from .models import *
 from .serializers import *
 
 
+
 def index(request):
     context={}
-    return render(request,"index.html",context)
+    return render(request,"login.html",context)
 def home(request):
     context={}
-    return render(request,"indexq.html",context)
+    return render(request,"index.html",context)
+def product(request):
+    context = {}
+    all = Product.objects.all()    
+    get_serializer = Product_Serializer(all, many = True)
+    return render(request,"products.html", {'all':all})
+
+
+def order(request):
+    context ={}
+    return render(request, "order.html", context)
+
 
 @api_view(['GET'])
 def get_products(request):
@@ -23,6 +35,7 @@ def get_products(request):
         "products":get_serializer.data
 
     })
+
 
 
 # Create your views here.
