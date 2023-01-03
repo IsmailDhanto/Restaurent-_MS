024750cbae960833f52a0570eea105dfdec33f7e
ismail <dhanto05@gmail.com>
2023-01-03 15:39:20 +0300

    commited

--- Backend/restuarent/views.py
+++ Backend/restuarent/views.py
@@ -13,15 +13,30 @@ def home(request):
     context={}
     return render(request,"index.html",context)
 def product(request):
-    context = {}
+    
     all = Product.objects.all()    
     get_serializer = Product_Serializer(all, many = True)
-    return render(request,"products.html", {'all':all})
+    context = {
+        "all":all
+    }
+    return render(request,"products.html", context)
 
 
 def order(request):
     context ={}
     return render(request, "order.html", context)
+
+
+
+
+def transaction(request):
+    context = {}
+    return render(request,"transaction.html", context)
+
+
+def staff(request):
+    context = {}
+    return render(request, "staff.html", context)
 
 
 @api_view(['GET'])
