fa1595ff58d914630a5196ec5e9d541990e0e111
ismail <dhanto05@gmail.com>
2023-01-05 10:01:43 +0300

    commited

--- Backend/restuarent/urls.py
+++ Backend/restuarent/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("product/",views.product, name="product"),
     path("order/",views.order, name="order"),
     path("transaction/",views.transaction, name="transaction"),
-    path("staff/",views.staff, name="staff")
+    path("staff/",views.staff, name="staff"),
+    path("post_product/",views.post_product, name='post_product')
 ]
