0728313840cf6a0f7f956de90b905ee419f8a480
ismail <dhanto05@gmail.com>
2023-01-05 10:07:12 +0300

    commited

--- Backend/resturent_api/settings.py
+++ Backend/resturent_api/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'resturent_api',
-    'restuarent'
+    'restuarent',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -128,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
